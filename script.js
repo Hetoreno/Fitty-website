b4f55c431e6aba70b49f05942bd7b6e39bfad24c
Hetoreno <hectortamriel@gmail.com>
2020-09-15 20:21:27 -0700

    updating

---
+++ script.js
@@ -0,0 +1,10 @@
+/* const hover = document.querySelector(".cat-img");
+hover.addEventListener("mouseenter",catPic);
+hover.addEventListener("mouseleave",catPic);
+
+function catPic(event){
+    const item= event.target;
+    console.log(item);
+
+    hover.classList.toggle("scale");
+} */
